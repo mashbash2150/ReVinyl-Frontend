c9eee39eaff4b17bec437a6715c3bd468ed35f94
Tyler <tylercaillet2121@gmail.com>
2022-11-14 11:10:09 -0500

    Login completed

---
+++ client/src/pages/Login.js
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { LoginUser } from '../services/Auth'
+import { Link } from 'react-router-dom'
+
+const Login = ({ toggleAuthenticated, setUser }) => {
+  const navigate = useNavigate()
+  const [formValues, setFormValues] = useState({ username: '', password: '' })
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const payload = await SigninUser(formValues)
+    setFormValues({ username: '', password: '' })
+    setUser(payload)
+    toggleAuthenticated(true)
+    navigate('/feed')
+  }
+
+  return (
+    <div className="signin col">
+      <div className="card-overlay centered">
+        <form className="col" onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="email">Username</label>
+            <input
+              onChange={handleChange}
+              name="username"
+              type="text"
+              placeholder="johndoe1234"
+              value={formValues.username}
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={handleChange}
+              type="password"
+              name="password"
+              value={formValues.password}
+              required
+            />
+          </div>
+          <button disabled={!formValues.username || !formValues.password}>
+            Sign In
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
