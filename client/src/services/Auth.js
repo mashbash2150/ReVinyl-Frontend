2858df32256889a997eaa5adc97c4245188a66e4
Tyler <tylercaillet2121@gmail.com>
2022-11-14 10:57:43 -0500

    auth.js completed

---
+++ client/src/services/Auth.js
@@ -0,0 +1,29 @@
+import Client from './api'
+
+export const RegisterUser = async (data) => {
+  try {
+    const res = await Client.post('/users/register', data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+export const LoginUser = async (data) => {
+  try {
+    const res = await Client.post('/users/login', data)
+    localStorage.setItem('token', res.data.token)
+    return res.data.user
+  } catch (error) {
+    throw error
+  }
+}
+
+export const CheckSession = async () => {
+  try {
+    const res = await Client.get('/users/session')
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
