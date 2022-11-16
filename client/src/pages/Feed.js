f3be83eb4605d66cb14297d87a79db08a45da71c
Kevin Blanco <kevinjblanco@gmail.com>
2022-11-16 17:08:16 -0500

    home page styling

--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -15,23 +15,25 @@ const Feed = ({ user, authenticated, vinylList }) => {
       {vinylList.map((record) => (
         <div className="vinyl-card">
         <div className="vinyl-text card" key={record.id}>
-          <div className="album-title">Title: <strong> {record.title}</strong></div>
-          <div className="album-details">
-          <div >Artist: {record.artist}</div>
-          <div>Genre: {record.genre}</div>
-          <div>Price: ${record.price}</div>
+    
+    
           </div>
-          <div>
+          <div className="large-circle">
+          <div className="album-title">Title: <strong> {record.title}</strong></div>
+          <div >Artist: {record.artist}</div>
             <div className="vinyl-img" key={record.id}>
               <div onClick={() => chooseVinyl(record)}>
                 <img src={record.image} alt="vinyl" />
-                
               </div>
-            </div>
-            <div className='status-banner'> * {record.status} *</div>
+  
+            <div>Genre: {record.genre}</div>
+          <div>Price: ${record.price}</div>
           </div>
+         
           </div>
+          <div className='status-banner'>  Looking To {record.status} </div>
         </div>
+        
       ))}
     </div>
   )
